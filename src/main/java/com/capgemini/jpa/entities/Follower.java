defff6d2f2f942b3286c55cfacf75087f9d47cc0
Viktor Moldovan <260311@student.pwr.edu.pl>
2024-04-11 20:13:13 +0200

    Finish tasks

--- src/main/java/com/capgemini/jpa/entities/Follower.java
+++ src/main/java/com/capgemini/jpa/entities/Follower.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @NamedEntityGraph(
         name = "follower-graph",
         attributeNodes = {
